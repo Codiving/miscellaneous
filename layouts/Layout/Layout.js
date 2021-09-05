28a7d242506025789e7ad5ec544664f65fbff672
Codiving <codivingmoon@gmail.com>
2021-09-05 17:53:17 +0900

    Update init rendering page

--- layouts/Layout/Layout.js
+++ layouts/Layout/Layout.js
@@ -4,7 +4,7 @@ import {
   unstable_createMuiStrictModeTheme
 } from "@material-ui/core/styles";
 import { MenuBar } from "layouts";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BloodType, Calculator, Couple, Mbti, Sudoku } from "../../components";
 import { menus } from "../../data";
 import Footer from "../Footer";
@@ -43,12 +43,22 @@ const Components = [
   }
 ];
 
+const initValue = [0, 0];
+
 const Layout = props => {
   // const { children } = props;
 
   const classes = useStyles();
   const [open, setOpen] = useState(false);
-  const [selected, setSelected] = useState([0, 0]);
+  const [selected, setSelected] = useState(initValue);
+
+  useEffect(() => {
+    setSelected(JSON.parse(localStorage.getItem("miscellaneous")) || initValue);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("miscellaneous", JSON.stringify(selected));
+  }, [selected]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -61,11 +71,11 @@ const Layout = props => {
           setSelected(selected);
         }}
       />
+
       {selected?.length &&
         Components.find(el => el.key === selected.join("")).components({
           className: classes.contents
         })}
-      {/* {children} */}
       <Footer />
     </ThemeProvider>
   );
