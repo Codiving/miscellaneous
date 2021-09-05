b3e37864395f70aecebe588ab08f11cadeccaf50
Codiving <codivingmoon@gmail.com>
2021-09-05 16:14:50 +0900

    Add Couple anniversary

--- components/Anniversary/Couple/Couple.js
+++ components/Anniversary/Couple/Couple.js
@@ -1,5 +1,69 @@
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { DateTime } from "luxon";
+import Head from "next/head";
+import Image from "next/image";
+import { useMemo } from "react";
+import { dates } from "./dates";
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  date: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    marginBottom: 20
+  },
+  hr: {
+    border: "1px solid",
+    width: "100%",
+    maxWidth: 600,
+    marginTop: 30
+  }
+}));
+
 const Couple = () => {
-  return <div>커플</div>;
+  const classes = useStyles();
+  const year = useMemo(() => DateTime.now().toFormat("yyyy"), []);
+
+  return (
+    <div className={classes.root}>
+      <Head>
+        <title>연인과의 기념일</title>
+        <meta
+          name="keyword"
+          content="연인, 기념일, 커플, 특별한 날, 날짜, 날짜 계산"
+        />
+        <meta
+          name="description"
+          content="연인, 커플간에 특별한 날을 확인할 수 있습니다."
+        />
+      </Head>
+
+      {dates.map((date, index) => {
+        const {
+          image,
+          name,
+          date: { month, day },
+          description
+        } = date;
+
+        return (
+          <div className={classes.date} key={name}>
+            <Typography variant="h6" component="h6">
+              {year}년 {month}월 {day}일 : {name}
+            </Typography>
+            <Typography>{description}</Typography>
+            <div>
+              <Image src={image} alt={name} width={600} height={400} />
+            </div>
+            <div className={classes.hr}></div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Couple;
---
+++ components/Anniversary/Couple/dates.js
@@ -0,0 +1,100 @@
+const dates = [
+  {
+    name: "다이어리데이",
+    date: { year: "", month: 1, day: 14 },
+    image: "/couple/diary.jpg",
+    description: "연인끼리 1년동안 작성할 다이어리를 선물하는 날"
+  },
+  {
+    name: "발렌타인데이",
+    date: { year: "", month: 2, day: 14 },
+    image: "/couple/chocolate.jpg",
+    description: "여자가 남자에게 초콜렛/꽃을 선물하는 날"
+  },
+  {
+    name: "삼겹살데이",
+    date: { year: "", month: 3, day: 3 },
+    image: "/couple/pork.jpg",
+    description: "친구끼리 삼겹살 먹는 날"
+  },
+  {
+    name: "화이트데이",
+    date: { year: "", month: 3, day: 14 },
+    image: "/couple/candy.jpg",
+    description: "남자가 여자에게 사탕/꽃을 선물하는 날"
+  },
+  {
+    name: "블랙데이",
+    date: { year: "", month: 4, day: 14 },
+    image: "/couple/jajangmyeon.jpg",
+    description: "솔로끼리 짜장면 먹으며 우는 날"
+  },
+  {
+    name: "로즈데이",
+    date: { year: "", month: 5, day: 14 },
+    image: "/couple/rose.jpg",
+    description: "연인/친구끼리 장미를 선물하는 날"
+  },
+  {
+    name: "부부의 날",
+    date: { year: "", month: 5, day: 21 },
+    image: "/couple/marriedcouple.jpg",
+    description: "부부끼리 맛있는 밥을 먹으며 기념하는 날"
+  },
+  {
+    name: "키스데이",
+    date: { year: "", month: 6, day: 14 },
+    image: "/couple/kiss.jpg",
+    description: "연인끼리 키스하는 날"
+  },
+  {
+    name: "실버데이",
+    date: { year: "", month: 7, day: 14 },
+    image: "/couple/silver.jpg",
+    description: "연인끼리 은반지를 주고 받는 날"
+  },
+  {
+    name: "뮤직데이",
+    date: { year: "", month: 8, day: 14 },
+    image: "/couple/music.jpg",
+    description: "연인끼리 음악과 꽃을 공유하는 날"
+  },
+  {
+    name: "포토데이",
+    date: { year: "", month: 9, day: 14 },
+    image: "/couple/photo.jpg",
+    description: "연인끼리 추억의 사진을 찍는 날"
+  },
+  {
+    name: "와인데이",
+    date: { year: "", month: 10, day: 14 },
+    image: "/couple/wine.jpg",
+    description: "연인끼리 와인을 마시는 날"
+  },
+  {
+    name: "빼뺴로데이",
+    date: { year: "", month: 11, day: 11 },
+    image: "/couple/movie.jpg",
+    description: "연인/친구끼리 빼빼로를 선물하는 날"
+  },
+  {
+    name: "무비데이",
+    date: { year: "", month: 11, day: 14 },
+    image: "/couple/movie.jpg",
+    description: "연인/친구끼를 영화를 보는 날"
+  },
+  {
+    name: "허그데이",
+    date: { year: "", month: 12, day: 14 },
+    image: "/couple/hug.jpg",
+    description: "연인끼리 사랑으로 안아주는 날"
+  },
+  {
+    name: "크리스마스",
+    date: { year: "", month: 12, day: 25 },
+    image: "/couple/christmas.jpg",
+    description: "모두한 행복한 크리스마스!"
+  }
+];
+
+export { dates };
